f44dd1769e7338842a0131e124defd9923c05bed
devwithfarshi <devwithfarshi@gmail.com>
2024-03-08 17:02:35 +0600

    login reduceer added and it work perfectly

--- src/redux/app/userSlice.js
+++ src/redux/app/userSlice.js
@@ -1,32 +1,59 @@
 // eslint-disable-next-line no-unused-vars
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios"
-export const loginUser=createAsyncThunk(
-    "auth/login",
-    ({username, password })=>{
-        axios.post("https://dummyjson.com/auth/login",{
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                   username: 'kminchelle',
-                   password: '0lelplR',
-            })
-        }).then(data=>data)
-            .catch(error=>error)
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async ({ username, password }) => {
+    try {
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          password,
+          // expiresInMins: 60, // optional
+        }),
+      });
+      const result = await res.json();
+      localStorage.setItem("userToken", result.token);
+      if (result.id) {
+        return result;
+      }
+      throw new Error(result.message);
+    } catch (error) {
+      console.log(error);
     }
-)
-
+  }
+);
 
 const initialState = {
   loading: false,
   error: null,
   userInfo: {},
-  token:null,
-success:false
+  token: null,
+  success: false,
 };
 const usersSlice = createSlice({
   name: "user",
   initialState,
-  reducers: { }
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.userInfo = action.payload;
+      state.token = action.payload.token;
+      state.success = true;
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.loading = false;
+      state.success = false;
+      state.error = action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
